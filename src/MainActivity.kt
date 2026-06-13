75246297eb0689545a1d85daab874f97119b82b3
Ndulechi <meremeze343@gmail.com>
2026-06-13 07:30:11 +0100

    Update MainActivity.kt

--- src/MainActivity.kt
+++ src/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ondrop
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -10,43 +11,60 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    LoginScreen()
-                }
-            }
+            OnDropApp()
         }
     }
 }
 
 @Composable
-fun LoginScreen() {
+fun OnDropApp() {
+    val navController = rememberNavController()
+    
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") { LoginScreen(navController) }
+        composable("signup") { SignUpScreen() }
+    }
+}
+
+@Composable
+fun LoginScreen(navController: androidx.navigation.NavController) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-
+    
     Column(
-        modifier = Modifier.fillMaxSize().padding(32.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxSize().padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text("Welcome to OnDrop", fontSize = 28.sp)
-        Spacer(modifier = Modifier.height(32.dp))
-        OutlinedTextField(value = email, onValueChange = { email = it }, label = { Text("Email") }, singleLine = true, modifier = Modifier.fillMaxWidth())
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(value = password, onValueChange = { password = it }, label = { Text("Password") }, singleLine = true, visualTransformation = PasswordVisualTransformation(), modifier = Modifier.fillMaxWidth())
-        Spacer(modifier = Modifier.height(24.dp))
-        Button(onClick = { /* TODO: Login logic */ }, modifier = Modifier.fillMaxWidth()) {
+        Text("OnDrop", style = MaterialTheme.typography.headlineLarge)
+        Spacer(Modifier.height(32.dp))
+        
+        OutlinedTextField(value = email, onValueChange = { email = it }, label = { Text("Email") }, modifier = Modifier.fillMaxWidth())
+        Spacer(Modifier.height(12.dp))
+        OutlinedTextField(
+            value = password, 
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(Modifier.height(24.dp))
+        Button(onClick = { /* Login logic later */ }, modifier = Modifier.fillMaxWidth()) {
             Text("Login")
         }
-        Spacer(modifier = Modifier.height(16.dp))
-TextButton(onClick = { /* TODO: Go to SignUp screen */ }) {
-    Text("Don't have an account? Sign Up")
-}
+        Spacer(Modifier.height(12.dp))
+        Text(
+            "Don't have an account? Sign Up",
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.clickable { navController.navigate("signup") }
+        )
     }
 }
